b45dcfe92cef36804534025538dbe7f334955bf4
michal.hrabovsky <michal.hrabovsky@accenture.com>
2020-11-05 21:25:22 +0100

    Fixed ports and operands

--- Src/main.c
+++ Src/main.c
@@ -38,29 +38,29 @@ int main(void)
   *((volatile uint32_t *) 0x40021000 + 0x00000014U) |= (uint32_t)(1 << 17);
 
   /*GPIOA pin3  setup*/
-  *((volatile uint32_t *)(GPIOA_BASE_ADDR)) &= ~(uint32_t)(0x3 << 6); 
-  *((volatile uint32_t *)(GPIOA_BASE_ADDR)) |= (uint32_t)(0x0 << 6);
+  *((volatile uint32_t *)(GPIOA_BASE_ADDR)) &= ~(uint32_t)(0x3 << 8); 
+  *((volatile uint32_t *)(GPIOA_BASE_ADDR)) |= (uint32_t)(0x0 << 8);
 
   /*GPIOB pin 3 and 6 setup*/
   /*GPIO MODER register*/
   //Set mode for pin 3
-  *((volatile uint32_t *)(GPIOB_BASE_ADDR) &= ~(uint32_t)(0x3 << 6);
-  *((volatile uint32_t *)(GPIOB_BASE_ADDR) |= (uint32_t)(1 << 6);
+  *((volatile uint32_t *)(GPIOB_BASE_ADDR)) &= ~(uint32_t)(0x3 << 8);
+  *((volatile uint32_t *)(GPIOB_BASE_ADDR)) |= (uint32_t)(1 << 8);
   //Set mode for pin 4
-  *((volatile uint32_t *)(GPIOB_BASE_ADDR) &= ~(uint32_t)(0x3 << 8);
+  *((volatile uint32_t *)(GPIOB_BASE_ADDR)) &= ~(uint32_t)(0x3 << 6);
 
   /*GPIO OTYPER register*/
-  *((volatile uint32_t *)((uint32_t)(0x48000400 + 0x04U))) &= ~(1 << 3);
+  *((volatile uint32_t *)((uint32_t)(0x48000400 + 0x04U))) &= ~(1 << 4);
 
   /*GPIO OSPEEDR register*/
   //Set Low speed for GPIOB pin 3
-  *((volatile uint32_t *)((uint32_t)(0x48000400 + 0x08U))) &= ~(0x3 << 6);
+  *((volatile uint32_t *)((uint32_t)(0x48000400 + 0x08U))) &= ~(0x3 << 8);
 
   /*GPIO PUPDR register, reset*/
   //Set pull up for GPIOB pin 4 (input)
-  *((volatile uint32_t *)((uint32_t)(0x48000400 + 0x0CU))) |= (1 << 8);
+  *((volatile uint32_t *)((uint32_t)(0x48000400 + 0x0CU))) |= (1 << 6);
   //Set no pull for GPIOB pin 3
-  *((volatile uint32_t *)((uint32_t)(0x48000400 + 0x0CU))) &= ~(0x3 << 6);
+  *((volatile uint32_t *)((uint32_t)(0x48000400 + 0x0CU))) &= ~(0x3 << 8);
 
   while (1)
   {
